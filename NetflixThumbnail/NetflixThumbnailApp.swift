4992f69b32492bbcd3c05c918d73acdef87c3226
youngseoc <cys020628@naver.com>
2025-03-22 06:13:05 +0900

    Initial Commit

---
+++ NetflixThumbnail/NetflixThumbnailApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixThumbnailApp.swift
+//  NetflixThumbnail
+//
+//  Created by 영석 on 3/22/25.
+//
+
+import SwiftUI
+
+@main
+struct NetflixThumbnailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NetflixThumbnailTests/NetflixThumbnailTests.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixThumbnailTests.swift
+//  NetflixThumbnailTests
+//
+//  Created by 영석 on 3/22/25.
+//
+
+import Testing
+@testable import NetflixThumbnail
+
+struct NetflixThumbnailTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
